e01324f5ea07ac5c44026d96c5813cfbf8192831
yangdy <d.y.21@163.com>
2019-02-27 09:52:27 +0800

    update 0.1
    Signed-off-by: yangdy <d.y.21@163.com>

--- src/com/soft/demo0227/Test.java
+++ src/com/soft/demo0227/Test.java
@@ -3,4 +3,11 @@ package com.soft.demo0227;
 public class Test {
     private String name;
     private String password;
+
+    public String getName() {
+        return name;
+    }
+    public String getPassword() {
+        return password;
+    }
 }
