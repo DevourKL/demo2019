1e82a28805720f998567cf9ebbd6bc4d3bdf9d76
yangdy <d.y.21@163.com>
2019-02-27 09:31:40 +0800

    first commit

---
+++ src/com/soft/demo0227/JavaDemo1.java
@@ -0,0 +1,12 @@
+package com.soft.demo0227;
+
+public class JavaDemo1 {
+    public static void main(String[] args){
+        setOut();
+        System.out.println("Holle Word!");
+
+    }
+    public static void setOut(){
+        System.out.println("Holle 123");
+    }
+}
