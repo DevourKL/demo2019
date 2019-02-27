28774e33aa9c96addfd770b6dc807f70f283a710
yangdy <d.y.21@163.com>
2019-02-27 12:41:00 +0800

    update 0.2
    Signed-off-by: yangdy <d.y.21@163.com>

--- src/com/soft/demo0227/Test.java
+++ src/com/soft/demo0227/Test.java
@@ -3,6 +3,24 @@ package com.soft.demo0227;
 public class Test {
     private String name;
     private String password;
+    private String 中;
+    private String 长度;
+
+    public String get长度() {
+        return 长度;
+    }
+
+    public void set长度(String 长度) {
+        this.长度 = 长度;
+    }
+
+    public String get中() {
+        return 中;
+    }
+
+    public void set中(String 中) {
+        this.中 = 中;
+    }
 
     public void setName(String name) {
         this.name = name;
