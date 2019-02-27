cc3090678b7fda4d94301c4692c7e3719eb9758e
yangdy <d.y.21@163.com>
2019-02-27 12:12:34 +0800

    first commit
    Signed-off-by: yangdy <d.y.21@163.com>

--- src/com/soft/demo0227/Test.java
+++ src/com/soft/demo0227/Test.java
@@ -4,7 +4,21 @@ public class Test {
     private String name;
     private String password;
 
+    public void setName(String name) {
+        this.name = name;
+        System.out.println("123");
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getName() {
         return name;
     }
+
 }
